3cd24bdf4c593c4fb864eda0e09e29be8b1fa67e
Wei <2356403837@qq.com>
2022-11-23 11:57:15 +0800

    试验任务委托详情项目title

--- src/views/hifar/hifar-environmental-test/task/components/TestBaseDetail.vue
+++ src/views/hifar/hifar-environmental-test/task/components/TestBaseDetail.vue
@@ -10,19 +10,29 @@
   <div style="height: 100%">
     <!-- 基本信息 -->
     <a-tabs v-model="activeTab" size="small" type="card" @change="handleTabsChange">
-      <a-tab-pane v-for="(item, index) in entrustInfo" :key="index"
-                  :tab="item.flag ?item.title : item.entrustCode "></a-tab-pane>
+      <a-tab-pane
+        v-for="(item, index) in entrustInfo"
+        :key="index"
+        :tab="item.flag ?item.title : item.entrustCode "></a-tab-pane>
     </a-tabs>
     <template v-if="entrustInfo[activeTab]['flag']">
-      <abnormal-record-table v-if="entrustInfo[activeTab]['type'] === 'exception'"
-                             ref="AbnormalRecordTable"
-                             :records="detailData" :style="{ marginTop: top ? top : '50px',height:'100%' }" isReadOnly/>
-      <termination-record-table v-if="entrustInfo[activeTab]['type'] === 'end'"
-                                ref="TerminationRecordTable"
-                                :records="detailData" :style="{ marginTop: top ? top : '50px',height:'100%' }"/>
-      <test-report-info v-if="entrustInfo[activeTab]['type'] === 'report'"
-                        ref='TestReportInfo' :style="{ marginTop: top ? top : '50px',height:'100%' }"
-                        :testId="detailData.id" class='autoHeight'/>
+      <abnormal-record-table
+        v-if="entrustInfo[activeTab]['type'] === 'exception'"
+        ref="AbnormalRecordTable"
+        :records="detailData"
+        :style="{ marginTop: top ? top : '50px',height:'100%' }"
+        isReadOnly/>
+      <termination-record-table
+        v-if="entrustInfo[activeTab]['type'] === 'end'"
+        ref="TerminationRecordTable"
+        :records="detailData"
+        :style="{ marginTop: top ? top : '50px',height:'100%' }"/>
+      <test-report-info
+        v-if="entrustInfo[activeTab]['type'] === 'report'"
+        ref="TestReportInfo"
+        :style="{ marginTop: top ? top : '50px',height:'100%' }"
+        :testId="detailData.id"
+        class="autoHeight"/>
     </template>
     <template v-else>
       <!-- 基本信息 -->
@@ -35,26 +45,32 @@
         :dataSource="detailData.testPieceInfo"/>
       <!-- 项目信息 -->
       <template v-for="(item,index) in projectInfo">
-        <project-detail-template :key="index" :model="item"
-                                 :title="'项目信息' + '(' + detailData.entrustInfo[index].entrustCode  + ')'"></project-detail-template>
+        <project-detail-template
+          :key="index"
+          :model="item"
+          :title="'项目信息' + '(' + item.entrustCode + ')'"></project-detail-template>
       </template>
       <!-- 试验信息 -->
-      <h-desc id="testInfo" ref="testInfo" lableWidth="110px" style="margin-top: 20px; margin-bottom: 20px"
-              title="试验信息">
+      <h-desc
+        id="testInfo"
+        ref="testInfo"
+        lableWidth="110px"
+        style="margin-top: 20px; margin-bottom: 20px"
+        title="试验信息">
         <h-desc-item label="试验设备">{{ detailData.equipName + '-' + detailData.equipModel || '--' }}</h-desc-item>
         <h-desc-item label="设备速率">{{ detailData.testRate || '--' }}</h-desc-item>
         <h-desc-item label="负责人">{{ detailData.chargeUserName || '--' }}</h-desc-item>
         <h-desc-item label="开始时间">{{
-            detailData.realStartTime && detailData.realStartTime != 0
-              ? moment(parseInt(detailData.realStartTime)).format('YYYY-MM-DD HH:mm:ss')
-              : '--'
-          }}
+          detailData.realStartTime && detailData.realStartTime != 0
+            ? moment(parseInt(detailData.realStartTime)).format('YYYY-MM-DD HH:mm:ss')
+            : '--'
+        }}
         </h-desc-item>
         <h-desc-item label="结束时间">{{
-            detailData.realEndTime && detailData.realEndTime != 0
-              ? moment(parseInt(detailData.realEndTime)).format('YYYY-MM-DD HH:mm:ss')
-              : '--'
-          }}
+          detailData.realEndTime && detailData.realEndTime != 0
+            ? moment(parseInt(detailData.realEndTime)).format('YYYY-MM-DD HH:mm:ss')
+            : '--'
+        }}
         </h-desc-item>
         <h-desc-item label="试验部门">{{ projectInfo.workName || '--' }}</h-desc-item>
         <h-desc-item :span="3" label="参试人员">
@@ -70,8 +86,13 @@
         <h-upload-file style="width: 100%" v-model="pictureData" isWriteRemarks :isEdit="false"></h-upload-file>
       </h-desc>
       <!-- 试前检查 -->
-      <h-desc id="testBeforCheck" ref="testBeforCheck" :bordered='false' lableWidth="110px"
-              style="margin-top: 20px; margin-bottom: 20px" title="试前检查">
+      <h-desc
+        id="testBeforCheck"
+        ref="testBeforCheck"
+        :bordered="false"
+        lableWidth="110px"
+        style="margin-top: 20px; margin-bottom: 20px"
+        title="试前检查">
         <h-vex-table
           ref="beforeCheckInfo"
           :columns="columns"
@@ -80,16 +101,21 @@
           bordered
           style="width: 100%; height: 200px"
         >
-        <span slot="itemRes" slot-scope="text, record">
-            <h-icon v-if="record.itemRes === '2'" class='success-text' type='icon-wancheng1'/>
-            <h-icon v-else-if="record.itemRes === '3'" class='danger-text' type='icon-chacha'/>
+          <span slot="itemRes" slot-scope="text, record">
+            <h-icon v-if="record.itemRes === '2'" class="success-text" type="icon-wancheng1"/>
+            <h-icon v-else-if="record.itemRes === '3'" class="danger-text" type="icon-chacha"/>
             <span v-else style="display:inline-block;width:100%;text-align: left;" v-text="record.itemRes"></span>
-        </span>
+          </span>
         </h-vex-table>
       </h-desc>
       <!-- 试中检查 -->
-      <h-desc id="testInCheck" ref="testInCheck" :bordered='false' lableWidth="110px"
-              style="margin-top: 20px; margin-bottom: 20px" title="试中检查">
+      <h-desc
+        id="testInCheck"
+        ref="testInCheck"
+        :bordered="false"
+        lableWidth="110px"
+        style="margin-top: 20px; margin-bottom: 20px"
+        title="试中检查">
         <h-vex-table
           ref="inCheckInfo"
           :columns="columns"
@@ -99,15 +125,20 @@
           style="width: 100%; height: 200px"
         >
           <span slot="itemRes" slot-scope="text, record">
-            <h-icon v-if="record.itemRes === '2'" class='success-text' type='icon-wancheng1'/>
-            <h-icon v-else-if="record.itemRes === '3'" class='danger-text' type='icon-chacha'/>
+            <h-icon v-if="record.itemRes === '2'" class="success-text" type="icon-wancheng1"/>
+            <h-icon v-else-if="record.itemRes === '3'" class="danger-text" type="icon-chacha"/>
             <span v-else style="display:inline-block;width:100%;text-align: left;" v-text="record.itemRes"></span>
           </span>
         </h-vex-table>
       </h-desc>
       <!-- 试后检查 -->
-      <h-desc id="testAfterCheck" ref="testAfterCheck" :bordered='false' lableWidth="110px"
-              style="margin-top: 20px; margin-bottom: 20px" title="试后检查">
+      <h-desc
+        id="testAfterCheck"
+        ref="testAfterCheck"
+        :bordered="false"
+        lableWidth="110px"
+        style="margin-top: 20px; margin-bottom: 20px"
+        title="试后检查">
         <h-vex-table
           ref="afterCheckInfo"
           :columns="columns"
@@ -116,19 +147,23 @@
           bordered
           style="width: 100%; height: 200px"
         >
-        <span slot="itemRes" slot-scope="text, record">
-          <h-icon v-if="record.itemRes === '2'" class='success-text' type='icon-wancheng1'/>
-            <h-icon v-else-if="record.itemRes === '3'" class='danger-text' type='icon-chacha'/>
+          <span slot="itemRes" slot-scope="text, record">
+            <h-icon v-if="record.itemRes === '2'" class="success-text" type="icon-wancheng1"/>
+            <h-icon v-else-if="record.itemRes === '3'" class="danger-text" type="icon-chacha"/>
             <span v-else style="display:inline-block;width:100%;text-align: left;" v-text="record.itemRes"></span>
-        </span>
+          </span>
         </h-vex-table>
       </h-desc>
       <!-- 试验数据 -->
       <h-desc id="testData" ref="testData" title="试验数据">
         <div style="height: 100%; width: 100%; overflow: auto; padding: 20px">
           <h-desc id="attachForm" :bordered="false">
-            <h-form ref="attachForm" v-model="model_attach" :column="1" :formData="attachData"
-                    style="width: 100%"/>
+            <h-form
+              ref="attachForm"
+              v-model="model_attach"
+              :column="1"
+              :formData="attachData"
+              style="width: 100%"/>
           </h-desc>
           <h-desc id="videoForm" :bordered="false">
             <h-form ref="videoForm" v-model="model_video" :column="1" :formData="videoData" style="width: 100%"/>
@@ -143,37 +178,41 @@
 
 <script>
 import moment from 'moment'
-import {downloadFile, getFileAccessHttpUrl, postAction} from '@api/manage'
+import { downloadFile, getFileAccessHttpUrl, postAction } from '@api/manage'
 import mixin from '@/views/hifar/mixin.js'
 import AbnormalRecordTable
   from '@/views/hifar/hifar-environmental-test/task/modules/components/detail/AbnormalRecordTable'
 import TerminationRecordTable
   from '@/views/hifar/hifar-environmental-test/task/modules/components/detail/TerminationRecordTable'
 import TestReportInfo from '@views/hifar/hifar-environmental-test/task/components/TestReportInfo'
-import TestEntrustReviewPdf from "@views/hifar/hifar-environmental-test/task/modules/TestEntrustReviewPdf";
-import DetailBaseInfo from "@views/hifar/hifar-environmental-test/entrustment/components/DetailBaseInfo";
-import PieceDetailTemplate from "@views/hifar/hifar-environmental-test/entrustment/components/PieceDetailTemplate";
-import ProjectDetailTemplate from "@views/hifar/hifar-environmental-test/entrustment/components/ProjectDetailTemplate";
+import TestEntrustReviewPdf from '@views/hifar/hifar-environmental-test/task/modules/TestEntrustReviewPdf';
+import DetailBaseInfo from '@views/hifar/hifar-environmental-test/entrustment/components/DetailBaseInfo';
+import PieceDetailTemplate from '@views/hifar/hifar-environmental-test/entrustment/components/PieceDetailTemplate';
+import ProjectDetailTemplate from '@views/hifar/hifar-environmental-test/entrustment/components/ProjectDetailTemplate';
 
 export default {
   components: {
     ProjectDetailTemplate,
     PieceDetailTemplate,
-    TestEntrustReviewPdf, AbnormalRecordTable, TerminationRecordTable, TestReportInfo, DetailBaseInfo
+    TestEntrustReviewPdf,
+AbnormalRecordTable,
+TerminationRecordTable,
+TestReportInfo,
+DetailBaseInfo
   },
   mixins: [mixin],
   props: {
     testId: {
       type: String,
-      default: '',
+      default: ''
     },
     viewDetailType: {
       type: String,
-      default: '',
+      default: ''
     },
     top: {
       type: String,
-      default: '',
+      default: ''
     },
     showExceptionAndEnd: {
       type: Boolean,
@@ -190,13 +229,13 @@ export default {
         CheckInfo: '/HfEnvTaskTestBusiness/queryTestCheckItem',
         // 试验数据
         attachList: '/MinioBusiness/listByRefId',
-        delete: '/MinioBusiness/logicRemoveById',
+        delete: '/MinioBusiness/logicRemoveById'
       },
       detailData: {},
       projectInfo: [],
       testEquipInfo: [],
       testPersonInfo: [],
-      entrustInfo: [{flag: false}],
+      entrustInfo: [{ flag: false }],
       entrustInfoItem: {},
       title: '',
       model_attach: {},
@@ -210,7 +249,7 @@ export default {
           minWidth: 10,
           customRender: (text, record) => {
             return text || '--'
-          },
+          }
         },
         {
           title: '检查项内容',
@@ -219,7 +258,7 @@ export default {
           minWidth: 10,
           customRender: (text, record) => {
             return text || '--'
-          },
+          }
         },
         {
           title: '检查项要求',
@@ -228,14 +267,14 @@ export default {
           minWidth: 25,
           customRender: (text, record) => {
             return text || '--'
-          },
+          }
         },
         {
           title: '检查结果',
           align: 'left',
           dataIndex: 'itemRes',
           minWidth: 10,
-          scopedSlots: {customRender: 'itemRes'},
+          scopedSlots: { customRender: 'itemRes' }
         },
         {
           title: '检查人',
@@ -243,7 +282,7 @@ export default {
           dataIndex: 'fillUserName',
           customRender: (text, record) => {
             return text || '--'
-          },
+          }
         },
         {
           title: '复核人',
@@ -251,12 +290,12 @@ export default {
           dataIndex: 'checkUserName',
           customRender: (text, record) => {
             return text || '--'
-          },
-        },
+          }
+        }
       ],
       // 试前检查
       beforeCheckInfo: () => {
-        return postAction(this.url.CheckInfo, {id: this.checkId}).then((res) => {
+        return postAction(this.url.CheckInfo, { id: this.checkId }).then((res) => {
           if (res.code === 200) {
             return res.data.beforeCheckInfo
           }
@@ -264,7 +303,7 @@ export default {
       },
       // 试中检查
       inCheckInfo: () => {
-        return postAction(this.url.CheckInfo, {id: this.checkId}).then((res) => {
+        return postAction(this.url.CheckInfo, { id: this.checkId }).then((res) => {
           if (res.code === 200) {
             return res.data.inCheckInfo
           }
@@ -272,7 +311,7 @@ export default {
       },
       // 试后检查
       afterCheckInfo: () => {
-        return postAction(this.url.CheckInfo, {id: this.checkId}).then((res) => {
+        return postAction(this.url.CheckInfo, { id: this.checkId }).then((res) => {
           if (res.code === 200) {
             return res.data.afterCheckInfo
           }
@@ -289,12 +328,12 @@ export default {
           component: (
             <h-upload-file
               isEdit={false}
-              v-decorator={['attachIds', {initialValue: []}]}
-              customParams={{refType: 'test_attach', refId: this.checkId}}
+              v-decorator={['attachIds', { initialValue: [] }]}
+              customParams={{ refType: 'test_attach', refId: this.checkId }}
               on-delete={this.handleDelete}
             />
-          ),
-        },
+          )
+        }
       ],
       // 视频
       videoData: [
@@ -305,13 +344,13 @@ export default {
           component: (
             <h-upload-file
               isEdit={false}
-              v-decorator={['attachIds', {initialValue: []}]}
-              customParams={{refType: 'test_video', refId: this.checkId}}
+              v-decorator={['attachIds', { initialValue: [] }]}
+              customParams={{ refType: 'test_video', refId: this.checkId }}
               on-delete={this.handleDelete}
             />
-          ),
-        },
-      ],
+          )
+        }
+      ]
     }
   },
   watch: {
@@ -324,8 +363,8 @@ export default {
           this.loadAttachData()
           this.loadVideoData()
         }
-      },
-    },
+      }
+    }
   },
   methods: {
     handleDownload(filePath, fileName) {
@@ -339,9 +378,9 @@ export default {
     },
     loadDetailData(id) {
       this.checkId = id
-      postAction(this.url.detail, {id: id, type: this.viewDetailType}).then((res) => {
+      postAction(this.url.detail, { id: id, type: this.viewDetailType }).then((res) => {
         if (res.code === 200) {
-          const {data} = res
+          const { data } = res
           let testEquipInfoArr = data.testEquipInfo
           let testPersonInfoArr = data.testPersonInfo
           let entrustInfoArr = data.entrustInfo
@@ -367,9 +406,9 @@ export default {
           let entrustInfo = entrustInfoArr
           if (this.showExceptionAndEnd) {
             entrustInfo.push(
-              {title: "报告信息", flag: true, type: "report"},
-              {title: "异常记录", flag: true, type: "exception"},
-              {title: "终止记录", flag: true, type: "end"},
+              { title: '报告信息', flag: true, type: 'report' },
+              { title: '异常记录', flag: true, type: 'exception' },
+              { title: '终止记录', flag: true, type: 'end' }
             )
           }
           this.entrustInfo = entrustInfo
@@ -378,9 +417,9 @@ export default {
     },
     // 图片
     loadImgData() {
-      postAction(this.url.attachList, {refType: 'test_picture', refId: this.checkId}).then((res) => {
+      postAction(this.url.attachList, { refType: 'test_picture', refId: this.checkId }).then((res) => {
         if (res.code === 200) {
-          const {data} = res
+          const { data } = res
           let fileArr = []
           if (data && data.length > 0) {
             data.forEach((item) => {
@@ -395,7 +434,7 @@ export default {
                 uploadTime: item.createTime,
                 secretLevel: item.secretLevel,
                 remarks: item.remarks,
-                type: item.viewType == 2 ? 'image/jpeg' : 'text/plain',
+                type: item.viewType == 2 ? 'image/jpeg' : 'text/plain'
               })
             })
           }
@@ -405,9 +444,9 @@ export default {
     },
     // 附件
     loadAttachData() {
-      postAction(this.url.attachList, {refType: 'test_attach', refId: this.checkId}).then((res) => {
+      postAction(this.url.attachList, { refType: 'test_attach', refId: this.checkId }).then((res) => {
         if (res.code === 200) {
-          const {data} = res
+          const { data } = res
           let fileArr = []
           let obj = {}
           if (data && data.length > 0) {
@@ -422,7 +461,7 @@ export default {
                 percent: 100,
                 uploadTime: item.createTime,
                 secretLevel: item.secretLevel,
-                type: item.viewType == 2 ? 'image/jpeg' : 'text/plain',
+                type: item.viewType == 2 ? 'image/jpeg' : 'text/plain'
               })
             })
           }
@@ -433,9 +472,9 @@ export default {
     },
     // 视频
     loadVideoData() {
-      postAction(this.url.attachList, {refType: 'test_video', refId: this.checkId}).then((res) => {
+      postAction(this.url.attachList, { refType: 'test_video', refId: this.checkId }).then((res) => {
         if (res.code === 200) {
-          const {data} = res
+          const { data } = res
           let fileArr = []
           let obj = {}
           if (data && data.length > 0) {
@@ -450,7 +489,7 @@ export default {
                 percent: 100,
                 uploadTime: item.createTime,
                 secretLevel: item.secretLevel,
-                type: item.viewType == 2 ? 'image/jpeg' : 'text/plain',
+                type: item.viewType == 2 ? 'image/jpeg' : 'text/plain'
               })
             })
           }
@@ -461,13 +500,13 @@ export default {
     },
     // 图片删除
     handleDelete(file, fileList) {
-      postAction(this.url.delete, {id: file.fileId}).then(res => {
+      postAction(this.url.delete, { id: file.fileId }).then(res => {
         if (res.code === 200) {
           this.$message.success('删除成功')
         }
       })
-    },
-  },
+    }
+  }
 }
 </script>
 <style lang='less' scoped>
