249319f636ede9ef6bd66294a2311a46180a0874
leiyuhang <602436674@qq.com>
2023-06-02 17:04:58 +0800

    update code

--- src/views/hifar/hifar-environmental-test/reports/mixin.js
+++ src/views/hifar/hifar-environmental-test/reports/mixin.js
@@ -205,7 +205,7 @@ export default {
         case 2:
           return '生成中';
         case 3:
-          return '已生成';
+          return '草稿';
         case 10:
           return '已提交';
         case 20:
